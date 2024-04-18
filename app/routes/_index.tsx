2afcb33b12b9cf4d6ab343c245b07bfe44bdf2c7
Yisehak Abreham <yisehak.aw@gmail.com>
2024-04-18 13:48:41 +0300

    initial commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { graphviz } from "d3-graphviz";
+import { useEffect, useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,34 +10,34 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [data, setData] = useState(`digraph {a -> b}`);
+
+  useEffect(() => {
+    if (!data) return;
+    const g = graphviz("#graph").renderDot(data);
+    g.height(document.getElementById("graph")?.clientHeight || 500);
+    g.width(document.getElementById("graph")?.clientWidth || 500);
+    g.fit(true);
+
+    const pasteListener = (e: ClipboardEvent) => {
+      e.preventDefault();
+      const content = e.clipboardData?.getData("text");
+      setData(content || "");
+    };
+    addEventListener("paste", pasteListener);
+    return () => removeEventListener("paste", pasteListener);
+  }, [data]);
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <div
+      id="graph"
+      style={{
+        height: "100vh",
+        width: "100vw",
+        position: "absolute",
+        top: 0,
+        left: 0,
+      }}
+    ></div>
   );
 }
